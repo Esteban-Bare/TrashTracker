5fbfda3ecc4493867cc15cd4f0a0b189833eda3b
damgin <ant529@outlook.fr>
2025-05-15 14:28:13 +0200

    ok

--- front/script.js
+++ front/script.js
@@ -1,13 +1,24 @@
 const userForm = document.querySelector(".user_info");
+const addfood = document.querySelector(".aliment");
+
+let list = [];
 
 
 
 
+addfood.addEventListener("submit", (event) => { //fonctionne
+    event.preventDefault(); // IMPORTANT pour ne pas recharger la page
+    const userfood = new FormData(event.target);
 
+    const newfood = {
+        aliment: userfood.get("food_search"),
+        quantite: userfood.get("food_quantity")
+    };
+  list.push(newfood);
+    console.log(list)
+});
 
-
-
-userForm.addEventListener("submit", (event) => {
+userForm.addEventListener("submit", (event) => {///fonctionne
     event.preventDefault(); // IMPORTANT pour ne pas recharger la page
 
     const userInfos = new FormData(event.target);
@@ -19,6 +30,4 @@ userForm.addEventListener("submit", (event) => {
         age: userInfos.get("age"),
         gender: userInfos.get("gender")
     };
-
-    console.log(user);
 });
