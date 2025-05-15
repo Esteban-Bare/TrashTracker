1f7b6197ad6dfe5745e42e4d1954d428de7908b0
Esteban <150131639+Esteban-Bare@users.noreply.github.com>
2025-05-15 16:11:16 +0200

    #2 Calculation of calories with data in the send email endpoint

--- Api/main.js
+++ Api/main.js
@@ -8,17 +8,42 @@ http.createServer(function (req, res) {
         req.on('data', chunk => {
             body += chunk.toString();
         });
-        req.on('end', () => {
+        req.on('end', async () => {
             const reqBody = JSON.parse(body);
-            const email = reqBody.email;
-            const gender = reqBody.gender;
-            const weight = reqBody.weight;
-            const height = reqBody.height;
-            const cals = reqBody.cals;
-            console.log(reqBody)
+            const email = reqBody.user.email;
+            const gender = reqBody.user.gender;
+            const weightKg = parseFloat(reqBody.user.weight); // Convert to number
+            const heightCm = parseFloat(reqBody.user.height) * 100; // Convert to number
+            const age = parseInt(reqBody.user.age); // Convert to integer
+            const foods = reqBody.foods;
+            console.log(heightCm)
 
+            console.log(calculateCalsOutOfFoods(foods))
+            const totalCals = await calculateCalsOutOfFoods(foods).then(
+                (result) => {
+                    console.log(result);
+                    return result;
+                }
+            ).catch((error) => {
+                    console.error('Error:', error);
+                }
+            );
+            const bmr = calculateBMR(gender, weightKg, heightCm, age);
+            if (totalCals > bmr) {
+                const body = {
+                    message : 'You are in a caloric surplus of ' + (totalCals - bmr) + ' calories.',
+                    calories : totalCals,
+                }
+                res.end(JSON.stringify(body));
+            } else {
+                const body = {
+                    message : 'You are in a caloric deficit of ' + (bmr - totalCals) + ' calories.',
+                    calories : totalCals,
+                }
+                res.end(JSON.stringify(body));
+            }
+            console.log(reqBody)
             // sendEmail(email);
-            res.end('Email sent to ' + email);
         });
     } else {
         res.end('Welcome to the server!');
@@ -34,8 +59,6 @@ const transporter = nodemailer.createTransport({
         pass: 'bsoljvsbxqktdsal'
     }
 });
-
-// Function to send an email
 
 function sendEmail(email) {
     const mailOptions = {
@@ -53,11 +76,42 @@ function sendEmail(email) {
     });
 }
 
-function aboveOrUnderCals(imc, cals, gender) {
-
+function calculateBMR(gender, weight, height, age) {
+    let bmr;
+    if (gender === '1') {
+        bmr = 10 * weight + 6.25 * height - 5 * age + 5;
+    } else if (gender === '2') {
+        bmr = 10 * weight + 6.25 * height - 5 * age - 161;
+    } else {
+        throw new Error("Gender must be 1 or 2");
+    }
+    return bmr;
 }
 
-function calculateIMC (weight, height) {
-    const imc = weight / (height * height);
-    return imc;
+async function calculateCalsOutOfFoods(foods) {
+    query = "";
+    foods.forEach(food => {
+        query += food.quantity + " " + food.aliment + " ";
+    });
+
+    const response = await fetch("https://trackapi.nutritionix.com/v2/natural/nutrients", {
+        method: 'POST',
+        headers: {
+            'x-app-id': 'f90ac08e',
+            'x-app-key': 'f8ef6bcd6e449c4daa31e4c723f41a2c',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            query: query,
+        })
+    });
+
+    const data = await response.json();
+
+    let totalCalories = 0;
+    data.foods.forEach(food => {
+        totalCalories += food.nf_calories;
+    });
+
+    return totalCalories;
 }
