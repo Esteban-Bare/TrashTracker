016c8d85077e9e2d6b101cee09908cbc8cfccb50
Esteban <150131639+Esteban-Bare@users.noreply.github.com>
2025-05-16 15:29:17 +0200

    #2 Refactor email sending logic and categorize caloric intake

--- Api/main.js
+++ Api/main.js
@@ -1,13 +1,16 @@
-var http = require('http');
-var nodemailer = require('nodemailer');
+const http = require('http');
+const nodemailer = require('nodemailer');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     if (req.url === '/send-email' && req.method === 'POST') {
+
         let body = '';
+
         req.on('data', chunk => {
             body += chunk.toString();
         });
+
         req.on('end', async () => {
             const reqBody = JSON.parse(body);
             const email = reqBody.user.email;
@@ -16,9 +19,9 @@ http.createServer(function (req, res) {
             const heightCm = parseFloat(reqBody.user.height) * 100; // Convert to number
             const age = parseInt(reqBody.user.age); // Convert to integer
             const foods = reqBody.foods;
-            console.log(heightCm)
+            // console.log(heightCm)
 
-            console.log(calculateCalsOutOfFoods(foods))
+            // console.log(calculateCalsOutOfFoods(foods))
             const totalCals = await calculateCalsOutOfFoods(foods).then(
                 (result) => {
                     console.log(result);
@@ -29,21 +32,22 @@ http.createServer(function (req, res) {
                 }
             );
             const bmr = calculateBMR(gender, weightKg, heightCm, age);
-            if (totalCals > bmr) {
-                const body = {
-                    message : 'You are in a caloric surplus of ' + (totalCals - bmr) + ' calories.',
-                    calories : totalCals,
-                }
-                res.end(JSON.stringify(body));
+
+            const category = categoryCaloriesIntake(totalCals, bmr);
+
+            if (category === 'very high') {
+                sendTrashTalkEmail(email, totalCals, bmr);
             } else {
-                const body = {
-                    message : 'You are in a caloric deficit of ' + (bmr - totalCals) + ' calories.',
-                    calories : totalCals,
-                }
-                res.end(JSON.stringify(body));
+                sendEmail(email, category, totalCals, bmr);
             }
-            console.log(reqBody)
-            // sendEmail(email);
+
+            const bodyRes = {
+                message: totalCals > bmr ? `You are in a caloric surplus of ${(totalCals - bmr)}` : `You are in a caloric deficit of ${(bmr - totalCals)}`,
+                calories : totalCals,
+                bmr : bmr,
+                category: category
+            }
+            res.end(JSON.stringify(bodyRes));
         });
     } else {
         res.end('Welcome to the server!');
@@ -60,14 +64,88 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-function sendEmail(email) {
+function sendEmail(email, category, totalCals, bmr) {
+    let subject;
+    let message;
+
+    switch (category) {
+        case 'very low':
+            subject = 'Very Low Caloric Intake';
+            message = `You are in a very low caloric intake of ${totalCals - bmr} calories out of a bmr of ${bmr}.`;
+            break;
+        case 'low':
+            subject = 'Low Caloric Intake';
+            message = `You are in a low caloric intake of ${totalCals - bmr} calories out of a bmr of ${bmr}.`;
+            break;
+        case 'normal':
+            subject = 'Normal Caloric Intake';
+            message = `You are in a normal caloric intake of ${totalCals - bmr} calories out of a bmr of ${bmr}.`;
+            break;
+        case 'high':
+            subject = 'High Caloric Intake';
+            message = `You are in a high caloric intake of ${totalCals - bmr} calories out of a bmr of ${bmr}.`;
+            break;
+        default:
+            subject = 'Caloric Intake';
+            message = `Your caloric intake is ${totalCals} calories out of a bmr of ${bmr}.`;
+            break;
+    }
+
+
     const mailOptions = {
         from: 'esteban.bare@laplateforme.io',
         to: email,
-        subject: 'Welcome to LaPlateforme',
-        text: 'Hello, welcome to LaPlateforme! We are glad to have you here.'
-    }
+        subject: subject,
+        text: message
+    };
+
     transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+function sendTrashTalkEmail(email,totalCals, bmr) {
+    const messages = [
+        {
+            subject: "Piece of SHIT, way too many calories",
+            text: `You consumed ${totalCals} cals! Thats way more that your bmr ${bmr}`
+        },
+        {
+            subject: "Are you serious? Overeating much?",
+            text: `Your intake of ${totalCals} calories is absurd compared to your bmr of ${bmr}. Get it together!`
+        },
+        {
+            subject: "Calorie overload detected!",
+            text: `Wow, ${totalCals} calories? That's way beyond your bmr of ${bmr}. Time to rethink your choices.`
+        },
+        {
+            subject: "Unbelievable calorie consumption!",
+            text: `You managed to consume ${totalCals} calories, completely ignoring your bmr of ${bmr}. Impressive, but not in a good way.`
+        },
+    ]
+
+    sendCustomEmail(email, messages[0].subject, messages[0].text);
+
+    [15, 30, 60].forEach((delay,index) => {
+        setTimeout(() => {
+            sendCustomEmail(email, messages[index + 1].subject, messages[index + 1].text);
+        }, delay * 1000);
+    })
+}
+
+function sendCustomEmail(email, subject, text) {
+    const mailOptions = {
+        from: 'esteban.bare@laplateforme.io',
+        to: email,
+        subject: subject,
+        text: text
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
             console.log(error);
         } else {
@@ -114,4 +192,25 @@ async function calculateCalsOutOfFoods(foods) {
     });
 
     return totalCalories;
+}
+
+function categoryCaloriesIntake(totalCals, bmr) {
+    const diff = totalCals - bmr;
+    const percentage = (diff / bmr) * 100;
+
+    if (percentage < -20) {
+        return 'very low';
+    }
+    else if (percentage < -10) {
+        return 'low';
+    }
+    else if (percentage < 10) {
+        return 'normal';
+    }
+    else if (percentage < 20) {
+        return 'high';
+    }
+    else {
+        return 'very high';
+    }
 }
